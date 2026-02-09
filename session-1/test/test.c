14c9ffebd250440fe65fb7f50dcf87224d2917c2
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-09 12:57:16 +0000

    S2W3S1 mucking about InsertChar

--- session-1/test/test.c
+++ session-1/test/test.c
@@ -1,9 +1,32 @@
 #include "tnal.c"
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
+
+int InsertChar(char** str, int strLen, char chr, int index);
 
 int main(void)
 {
     printf("a() returns %i\n",a());
+
+    char* str="hello";
+    InsertChar(&str, 5, '!', 5);
+    printf("str: %s",str);
+    free(str);
     return 0;
+}
+
+int InsertChar(char** str, int strLen, char chr, int index)
+{
+    char* str2=malloc(sizeof(char)*(strLen+1));
+    for(int i=0;i<index;i++)
+    {
+        str2[i]=(*str)[i];
+    }
+    str2[index]=chr;
+    for(int i=index+1;i<strLen;i++)
+    {
+        str2[i]=(*str)[i-1];
+    }
+    free((*str));
+    str=&str2;
 }
