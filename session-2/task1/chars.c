d44dbe7329a82e5ad0ef5e2ef607382127b65964
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-13 09:58:13 +0000

    S2W3S2 task1

--- session-2/task1/chars.c
+++ session-2/task1/chars.c
@@ -6,14 +6,18 @@
 
 #include <stdio.h>
 
-int main( void ) {
-    char c = 'a';
-    int k = 95;
+int main(void)
+{
+    char c='a';
+    int k=95;
+    int j=0x65;
 
     printf("Display char as char: %c\n",c);
     printf("Display char as int: %d\n",c);
     printf("Display int as int: %d\n",k);
     printf("Display int as char: %c\n",k);
+    printf("Display hex as int: %d\n",j);
+    printf("Display hex as char: %c\n",j);
 
     return 0;
 }
