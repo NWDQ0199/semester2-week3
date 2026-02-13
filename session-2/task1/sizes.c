87b94c7e72da8a11b38b431cedfdf466ca87e813
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-13 09:55:31 +0000

    S2W3S2 task1

--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -11,10 +11,19 @@ int main( void ) {
     char testChar;
     int testInt;
     float testFloat;
+    short int testShortInt;
+    long int testLongInt;
+    long long int testLongLongInt;
+    double testDouble;
+    long double testLongDouble;
 
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
-
+    printf("Short int size is %ld bytes\n",sizeof(testShortInt));
+    printf("Long int size is %ld bytes\n",sizeof(testLongInt));
+    printf("Long long int size is %ld bytes\n",sizeof(testLongLongInt)); //same as long
+    printf("Double size is %ld bytes\n",sizeof(testDouble));
+    printf("Long double size is %ld bytes\n",sizeof(testLongDouble)); //actually length of 2 doubles
     return 0;
 }
