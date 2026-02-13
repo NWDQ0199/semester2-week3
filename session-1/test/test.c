19293bcc533456b07aa9d95bca953fbe48b0ab47
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-02-13 09:19:20 +0000

    insertchar

--- session-1/test/test.c
+++ session-1/test/test.c
@@ -17,16 +17,17 @@ int main(void)
 
 int InsertChar(char** str, int strLen, char chr, int index)
 {
-    char* str2=malloc(sizeof(char)*(strLen+1));
+    char* str2=malloc(sizeof(char)*(strLen+1)); //strLen plus \0
+    char* strOrig=*str;
     for(int i=0;i<index;i++)
     {
-        str2[i]=(*str)[i];
+        str2[i]=strOrig[i];
     }
     str2[index]=chr;
     for(int i=index+1;i<strLen;i++)
     {
-        str2[i]=(*str)[i-1];
+        str2[i]=strOrig[i-1];
     }
-    free((*str));
+    //free(str);
     str=&str2;
 }
